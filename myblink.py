25edad82c50e4e68a46c2e99709604806a3946bb
Chris Muhs <9286624+cdmuhs@users.noreply.github.com>
2020-08-09 21:25:25 -0700

    Create a simple blinky program

---
+++ myblink.py
@@ -0,0 +1,14 @@
+# Simple "Blink" script to flash a LED every second.
+
+import pyfirmata
+import time
+
+# Establish serial connection. FInd the port in Arduino IDE.
+board = pyfirmata.Arduino('/dev/ttyACM0')
+
+# Every second, flash the LED on digital pin 13
+while True:
+    board.digital[13].write(1)
+    time.sleep(1)
+    board.digital[13].write(0)
+    time.sleep(1)
